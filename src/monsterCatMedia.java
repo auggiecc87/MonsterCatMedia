b7bf7c236d44859dd7b16e02510ed2cfdc72d540
auggiecc87 <brian.gladden@BGladden-LT1.youdictate.local>
2012-07-27 18:17:33 -0500

    added basic java project that isn't compiling

---
+++ src/monsterCatMedia.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brian.gladden
+ * Date: 7/27/12
+ * Time: 6:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class monsterCatMedia {
+}
